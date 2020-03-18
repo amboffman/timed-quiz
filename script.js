57f49abe4e1419ebd27d9a134d5c2bca16cf29d1
Anthony Boffman <amarrokalboffman@gmail.com>
2020-03-17 18:20:53 -0700

    Add framework intro page and first question

---
+++ script.js
@@ -0,0 +1,49 @@
+var startQuizButton = document.getElementById("start-quiz");
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("time-left");
+var header = document.getElementById("header");
+var info = document.getElementById("info");
+var viewHighScores = document.getElementById("view-high-scores");
+var buttons = document.querySelector(".buttons");
+var questions = ["Commonly used data types do NOT include:","The condition in an if/else sttement is enclosed within ______","Arrays in javascript can be used to store _____.","String values must be enclosed within ______ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
+var answers = ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers", "1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"];
+function createAnswerButtons(){
+    var answerButton1 = document.createElement("button");
+    answerButton1.textContent = answers[0];
+    document.body.append(answerButton1);
+    answerButton1.className = "btn btn-primary text-center";
+    answerButton1.setAttribute("style", "display:block; margin:20px auto; text-align:center");
+    
+    var answerButton2 = document.createElement("button");
+    answerButton2.textContent = answers[1];
+    document.body.append(answerButton2);
+    answerButton2.className = "btn btn-primary text-center";
+    answerButton2.setAttribute("style", "display:block; margin:20px auto; text-align:center");
+    
+    var answerButton3 = document.createElement("button");
+    answerButton3.textContent = answers[2];
+    document.body.append(answerButton3);
+    answerButton3.className = "btn btn-primary text-center";
+    answerButton3.setAttribute("style", "display:block; margin:20px auto; text-align:center");
+    
+    var answerButton4 = document.createElement("button");
+    answerButton4.textContent = answers[3];
+    document.body.append(answerButton4);
+    answerButton4.className = "btn btn-primary text-center";
+    answerButton4.setAttribute("style", "display:block; margin:20px auto; text-align:center");
+}
+// when the user clicks start quiz
+startQuizButton.addEventListener("click", startQuiz)
+    function startQuiz(){
+        // change h1 to left aligned
+        header.style.textAlign = "left";
+        // change h1 to question 1
+        // remove the p tag
+        header.textContent = questions[0];
+        info.remove();
+        // remove the start quiz button
+        startQuizButton.remove();
+        // add answer buttons with answer text
+        createAnswerButtons();
+    
+    }
